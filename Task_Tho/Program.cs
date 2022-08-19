ed0bd842fb670ba0fbfbce8ee64da071e7712ca8
Анастасия <nastenka_slastenka2022@mail.ru>
2022-08-20 01:01:34 +0800

    add Task Tho

---
+++ Task_Tho/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+Console.WriteLine("Введите длину массива: ");
+
+int lengthArray = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [lengthArray];
+int sumOddPositionsElem = 0;
+
+for(int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(-9, 9);
+    if(i % 2 != 0){
+        sumOddPositionsElem += array[i];
+    }
+}
+
+Console.WriteLine("Элементы массива : ");
+
+foreach(int elem in array){
+    Console.Write($"{elem}, ");
+}
+Console.WriteLine();
+
+Console.WriteLine($"Сумма чисел находящихся на нечётных позициях = {sumOddPositionsElem}");
